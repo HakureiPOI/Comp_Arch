7b5bfc814bf4d8a8f614317ddc94a793ce0eec0a
POI Hakurei <hakureipoi@qq.com>
2024-12-07 11:48:39 +0000

    lab2_gemm_baseline made

---
+++ lab2/build/src/lab2/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /workspace/Comp_Arch/lab2/src/lab2
+# Build directory: /workspace/Comp_Arch/lab2/build/src/lab2
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("/workspace/Comp_Arch/lab2/build/src/lab2/lab2_gemm_kernel_opt_loop.unittest[1]_include.cmake")
+include("/workspace/Comp_Arch/lab2/build/src/lab2/lab2_gemm_kernel_opt_prefetch.unittest[1]_include.cmake")
+subdirs("../../_deps/googletest-build")
---
+++ lab2/build/src/lab2/lab2_gemm_kernel_opt_loop.unittest[1]_include.cmake
@@ -0,0 +1,5 @@
+if(EXISTS "/workspace/Comp_Arch/lab2/build/src/lab2/lab2_gemm_kernel_opt_loop.unittest[1]_tests.cmake")
+  include("/workspace/Comp_Arch/lab2/build/src/lab2/lab2_gemm_kernel_opt_loop.unittest[1]_tests.cmake")
+else()
+  add_test(lab2_gemm_kernel_opt_loop.unittest_NOT_BUILT lab2_gemm_kernel_opt_loop.unittest_NOT_BUILT)
+endif()
