0b99c545be6bb35f588ec06f4262a096c5426ed6
HakureiPOI <114349635+HakureiPOI@users.noreply.github.com>
2024-12-25 15:22:13 +0800

    Update testbench_lab5.sh

--- lab4-5/testbench_lab5.sh
+++ lab4-5/testbench_lab5.sh
@@ -19,12 +19,12 @@ function compile() {
 }
 
 function test() {
-    for tile_width in 16 32 64; do  # 测试不同块大小
+    for tile_width in 16 32 64 128; do  # 测试不同块大小
         echo "=======================================" | tee -a results.log
         echo "Testing with TILE_WIDTH=$tile_width" | tee -a results.log
         echo "=======================================" | tee -a results.log
         compile $1 $tile_width  # 传入当前的块大小
-        for matrix_size in 128 256 512 1024; do  # 测试不同矩阵大小
+        for matrix_size in 128 256 512 1024 2048; do  # 测试不同矩阵大小
             echo "matrix_size: $matrix_size, TILE_WIDTH: $tile_width" | tee -a results.log
             ./a.out 0 $matrix_size | tee -a results.log
             echo "--------------------------------" | tee -a results.log
