2aa994f078a86cbee5af4033147a5ac925cf62fa
HakureiPOI <114349635+HakureiPOI@users.noreply.github.com>
2024-12-25 15:26:07 +0800

    Update testbench_lab5.sh

--- lab4-5/testbench_lab5.sh
+++ lab4-5/testbench_lab5.sh
@@ -19,7 +19,7 @@ function compile() {
 }
 
 function test() {
-    for tile_width in 16 32 64 128; do  # 测试不同块大小
+    for tile_width in 16 32 64; do  # 测试不同块大小
         echo "=======================================" | tee -a results.log
         echo "Testing with TILE_WIDTH=$tile_width" | tee -a results.log
         echo "=======================================" | tee -a results.log
