73a32dfdd50c3bf711a4bde7af0c685c43d27a89
HakureiPOI <114349635+HakureiPOI@users.noreply.github.com>
2024-12-25 14:09:36 +0800

    Update compile_with_tile_size.sh

--- lab4-5/compile_with_tile_size.sh
+++ lab4-5/compile_with_tile_size.sh
@@ -1,6 +1,6 @@
 function compile_with_tile_size() {
     local tile_size=$1
-    nvcc -arch=sm_75 -L/usr/local/cuda/lib64 -lcublas ./matrix_mul.cu -DTILE_SIZE=$tile_size -o $tile_size
+    nvcc -arch=sm_75 -L/usr/local/cuda/lib64 -lcublas ./matrix_mul_lab4.cu -DTILE_SIZE=$tile_size -o $tile_size
     ./$tile_size 0 1000
 }
 
